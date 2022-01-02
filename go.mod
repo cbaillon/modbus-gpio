1a4ddf1e47e5bb5f3d4ef451fa31fce76fcaaa7c
Christophe <christophe.baillon@sogilis.com>
2022-01-02 20:22:51 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module rgpio
+
+go 1.17
+
+require github.com/stianeikeland/go-rpio v4.2.0+incompatible
---
+++ rgpio.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/stianeikeland/go-rpio"
+)
+
+func main() {
+	fmt.Println("rgpio starting")
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	pin := rpio.Pin(10)
+	pin.Output()
+
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second)
+	}
+}
