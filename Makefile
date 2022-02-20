5306a8d22075509ddde4bfd1a10d9a723bd15750
Christophe Baillon <christophe.baillon@sogilis.com>
2022-02-20 17:22:41 +0000

    Create a Makefile Add a create_gpio_permissions target in the Makefile to set correct permissions to the current user launchin the target on the device.

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+
+
+build:
+	go build rgpio.go
+
+create_gpio_permissions: # Create permissions for the user launching this Makefile
+	sudo python3 create_gpio_user_permissions.py $(USER)
+	sudo udevadm trigger
--- create_gpio_user_permissions.py
+++ create_gpio_user_permissions.py
@@ -1,17 +1,24 @@
+# This script comes from: https://github.com/wuestkamp/raspberry-gpio-python/blob/master/create_gpio_user_permissions.py
+
 import grp
+import sys
 import subprocess
+
 
 def ensure_gpiogroup():
     try:
         grp.getgrnam('gpio')
+        print('GPIO group alread exists')
     except KeyError:
         print('GPIO group does not exist - creating...')
         subprocess.call(['groupadd', '-f', '-r', 'gpio'])
-        subprocess.call(['adduser', 'pi', 'gpio'])
-        # in future, also for groups:
-        #   spi
-        #   i2c
-        add_udev_rules()
+       
+    
+def add_user_to_gpio_group(user):
+    subprocess.call(['adduser', user, 'gpio'])
+    # in future, also for groups:
+    #   spi
+    #   i2c
 
 def add_udev_rules():
     with open('/etc/udev/rules.d/99-gpio.rules','w') as f:
@@ -21,5 +28,13 @@ SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add", PROGRAM="/bin/sh -c 'chown ro
 """)
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Syntax: create_gpio_user_permissions.py USER_NAME')
+        sys.exit(1)
+
+    username = sys.argv[1]
+    print("Creating GPIO permissions for user ", username, "...")
     ensure_gpiogroup()
+    add_user_to_gpio_group(username)
+    add_udev_rules()
 
